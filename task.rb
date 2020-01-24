214813e3e7d8a054a58f3e26a2907067d7b30d53
KawazuKento <myv@outlook.jp>
2020-01-24 21:47:52 +0900

    q５まで回答

--- task.rb
+++ task.rb
@@ -5,7 +5,9 @@ def q1
   names = ["田中", "佐藤", "佐々木", "高橋"]
 
   # 以下に回答を記載
+  names << ("斎藤")
 
+  puts names
 end
 
 def q2
@@ -13,13 +15,16 @@ def q2
   array2 = %w(bird bat tiger)
 
   # 以下に回答を記載
-
+  array = array1 + array2
+  puts array
 end
 
 def q3
   numbers = [1, 5, 8, 10, 2, 3, 2, 3, 3, 1, 4, 5, 9]
 
   # 以下に回答を記載
+  count_3 = numbers.count 3
+  p count_3
 
 end
 
@@ -27,7 +32,8 @@ def q4
   sports = ["サッカー", "フットサル", nil, "野球", "バスケ", nil, "バレー"]
 
   # 以下に回答を記載
-
+  sports.compact!
+  puts sports
 end
 
 def q5
@@ -35,7 +41,8 @@ def q5
   array2 = [1, 5, 8, 10]
 
   # 以下に回答を記載
-
+  p array1.empty?
+  p array2.empty?
 end
 
 def q6
